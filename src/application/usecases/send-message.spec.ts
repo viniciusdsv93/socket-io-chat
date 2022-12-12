7c35730a8705928e9e43b68577a9b1f965225c49
Vinicius Verissimo <viniciusdsv93@gmail.com>
2022-12-11 22:13:24 -0300

    refactor: change spy name to appendFileSpy

--- src/application/usecases/send-message.spec.ts
+++ src/application/usecases/send-message.spec.ts
@@ -4,13 +4,13 @@ import { SendMessage } from "./send-message";
 describe('Send Message', () => {
   test('Should call fs.writeFile with correct values', () => {
     const sut = new SendMessage()
-    const writeFileSpy = jest.spyOn(fs, 'appendFile')
+    const appendFileSpy = jest.spyOn(fs, 'appendFile')
     sut.send({
       date: new Date().toLocaleString(),
       content: 'test_content',
       receiver: 'receiver_user',
       sender: 'sender_user'
     })
-    expect(writeFileSpy).toHaveBeenCalledWith(expect.anything(), `${new Date().toLocaleString()} content: test_content receiver: receiver_user sender: sender_user\n`, expect.anything())
+    expect(appendFileSpy).toHaveBeenCalledWith(expect.anything(), `${new Date().toLocaleString()} content: test_content receiver: receiver_user sender: sender_user\n`, expect.anything())
   });
 });
